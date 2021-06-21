d6eef033db1abb1b4afed3f09b5519a63d4bfdf2
Slarkerino Company Desktop <307221214@qq.com>
2021-06-21 15:57:07 +0800

    Add MultiTenancy To Product Unit

--- modules/EasyAbp.EShop.Products/src/EasyAbp.EShop.Products.Domain/EasyAbp/EShop/Products/Products/ProductUnit.cs
+++ modules/EasyAbp.EShop.Products/src/EasyAbp.EShop.Products.Domain/EasyAbp/EShop/Products/Products/ProductUnit.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
 
 namespace EasyAbp.EShop.Products.Products
 {
-    public class ProductUnit : AuditedEntity<Guid>, IProductUnit
+    public class ProductUnit : AuditedEntity<Guid>, IProductUnit, IMultiTenant
     {
         [NotNull]
         public virtual string DisplayName { get; protected set; }
 
+        public Guid? TenantId { get; protected set; }
 
         protected ProductUnit()
         {
