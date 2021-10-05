de6174b3d609ab0e0bb265d2e10fc891453683f7
Slarkerino Company Desktop <wanjackcool@qq.com>
2021-10-06 00:03:20 +0800

    Update Version

--- modules/EasyAbp.EShop.Products/src/EasyAbp.EShop.Products.Application/EasyAbp/EShop/Products/Products/ProductSkuAppService.cs
+++ modules/EasyAbp.EShop.Products/src/EasyAbp.EShop.Products.Application/EasyAbp/EShop/Products/Products/ProductSkuAppService.cs
@@ -1,20 +1,12 @@
-using EasyAbp.EShop.Products.Options;
-using EasyAbp.EShop.Products.Permissions;
-using EasyAbp.EShop.Products.ProductCategories;
-using EasyAbp.EShop.Products.ProductCategories.Dtos;
-using EasyAbp.EShop.Products.Products.CacheItems;
 using EasyAbp.EShop.Products.Products.Dtos;
-using EasyAbp.EShop.Stores.Authorization;
-using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Volo.Abp;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
-using Volo.Abp.Caching;
-using Volo.Abp.Domain.Entities;
+using Volo.Abp.Data;
+using Volo.Abp.MultiTenancy;
 
 namespace EasyAbp.EShop.Products.Products
 {
@@ -26,28 +18,33 @@ namespace EasyAbp.EShop.Products.Products
         private readonly IProductInventoryProvider _productInventoryProvider;
         private readonly IProductSkuRepository _repository;
         private readonly IProductManager _productManager;
+        private readonly IDataFilter<IMultiTenant> _tenantFilter;
 
         public ProductSkuAppService(
             IProductInventoryProvider productInventoryProvider,
             IProductSkuRepository repository,
-            IProductManager productManager) : base(repository)
+            IProductManager productManager, IDataFilter<IMultiTenant> tenantFilter) : base(repository)
         {
             _productInventoryProvider = productInventoryProvider;
             _repository = repository;
             _productManager = productManager;
+            _tenantFilter = tenantFilter;
         }
 
         protected override async Task<IQueryable<ProductSku>> CreateFilteredQueryAsync(GetProductSkuListInput input)
         {
-            var query = await _repository.WithDetailsAsync();
+            using (_tenantFilter.Disable())
+            {
+                var query = await _repository.WithDetailsAsync();
 
-            return query
-                .WhereIf(input.StoreId.HasValue, x => x.Product.StoreId == input.StoreId)
-                //.WhereIf(!input.ShowHidden, x => !x.Product.IsHidden)
-                //.WhereIf(input.IsPublished.HasValue, x => x.Product.IsPublished == input.IsPublished.Value)
-                .WhereIf(!input.Filter.IsNullOrEmpty(), x => x.Name.Contains(input.Filter))
-                .WhereIf(input.MinimumPrice.HasValue, x => x.Price >= input.MinimumPrice.Value)
-                .WhereIf(input.MaximumPrice.HasValue, x => x.Price <= input.MaximumPrice.Value);
+                return query
+                    .WhereIf(input.StoreId.HasValue, x => x.Product.StoreId == input.StoreId)
+                    //.WhereIf(!input.ShowHidden, x => !x.Product.IsHidden)
+                    //.WhereIf(input.IsPublished.HasValue, x => x.Product.IsPublished == input.IsPublished.Value)
+                    .WhereIf(!input.Filter.IsNullOrEmpty(), x => x.Name.Contains(input.Filter))
+                    .WhereIf(input.MinimumPrice.HasValue, x => x.Price >= input.MinimumPrice.Value)
+                    .WhereIf(input.MaximumPrice.HasValue, x => x.Price <= input.MaximumPrice.Value);
+            }
         }
 
         public override async Task<PagedResultDto<ProductSkuDto>> GetListAsync(GetProductSkuListInput input)
@@ -125,12 +122,10 @@ namespace EasyAbp.EShop.Products.Products
 
         protected virtual async Task<ProductSkuDto> LoadDtoPriceDataAsync(ProductSku productSku, ProductSkuDto productSkuDto)
         {
+            var priceDataModel = await _productManager.GetProductPriceAsync(productSku.Product, productSku);
 
-                var priceDataModel = await _productManager.GetProductPriceAsync(productSku.Product, productSku);
-
-                productSkuDto.Price = priceDataModel.Price;
-                productSkuDto.DiscountedPrice = priceDataModel.DiscountedPrice;
-            
+            productSkuDto.Price = priceDataModel.Price;
+            productSkuDto.DiscountedPrice = priceDataModel.DiscountedPrice;
 
             return productSkuDto;
         }
@@ -150,6 +145,5 @@ namespace EasyAbp.EShop.Products.Products
 
             return Task.FromResult(productDto);
         }
-
     }
 }
